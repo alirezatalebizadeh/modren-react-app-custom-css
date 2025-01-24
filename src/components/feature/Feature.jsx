e000d42220b0e545ed06347cbf08a251dde8058f
Alireza Talebizadeh <Alireza.talebizadeh78@gmail.com>
2025-01-24 22:03:06 +0330

    create design of footer and blog and cta section

--- src/components/feature/Feature.jsx
+++ src/components/feature/Feature.jsx
@@ -3,12 +3,12 @@ import './feature.css'
 export default function Feature({ title, text }) {
     return (
         <div className='gpt3__features-container__feature'>
-            <div className="gpt3__features-container__feature-title">
+            <div className="gpt3__features-container__feature-title" style={{ flex: 2 }}>
                 <div />
                 <h1>{title}</h1>
             </div>
-            <div className="gpt3__features-container__feature-text">
-                <p style={{color:"#81AFDD"}}>{text}</p>
+            <div className="gpt3__features-container__feature-text" style={{ flex: 2 }}>
+                <p style={{ color: "#81AFDD" }}>{text}</p>
             </div>
         </div>
     )
--- src/containers/footer/Footer.jsx
+++ src/containers/footer/Footer.jsx
@@ -1,8 +1,49 @@
 import React from 'react'
+import Gpt3Logo from '../../assets/logo.svg'
 import './footer.css'
+
+
 
 export default function Footer() {
   return (
-    <div>Footer</div>
+    <div className='gpt3__footer'>
+      <div className="gpt3__footer-heading">
+        <h1 className="gradient__text">Do you want to step in to the future before others</h1>
+      </div>
+
+      <div className="gpt3__footer-btn">
+        <button >Request Early Access</button>
+      </div>
+
+      <div className="gpt3__footer-links">
+        <div className="gpt3__footer-links_logo">
+          <img src={Gpt3Logo} alt="gpt3_logo" />
+          <p style={{ color: "#fff" }}>Crechterwoord K12 182 DK Alknjkcb, <br /> All Rights Reserved</p>
+        </div>
+        <div className="gpt3__footer-links_div">
+          <h4>Links</h4>
+          <a href='#'>Overons</a>
+          <a href='#'>Social Media</a>
+          <a href='#'>Counters</a>
+          <a href='#'>Contact</a>
+        </div>
+        <div className="gpt3__footer-links_div">
+          <h4>Company</h4>
+          <a href='#'>Terms & Conditions </a>
+          <a href='#'>Privacy Policy</a>
+          <a href='#'>Contact</a>
+        </div>
+        <div className="gpt3__footer-links_div">
+          <h4>Get in touch</h4>
+          <a href='#'>Crechterwoord K12 182 DK Alknjkcb</a>
+          <a href='#'>085-132567</a>
+          <a href='#'>info@payme.net</a>
+        </div>
+      </div>
+
+      <div className="gpt3__footer-copyright">
+        <p >@2021 GPT-3. All rights reserved.</p>
+      </div>
+    </div>
   )
 }
